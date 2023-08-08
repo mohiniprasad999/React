c3c075872ac9e89199454f7a209003309396da7f
mohiniprasad <mohini.prasad@stackinfinite.com>
2023-08-08 18:44:51 +0530

    login form UI

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Login from './components/Login';
+
+
+function App() {
+
+  return (
+    <>
+      <div className="heading">
+        <h1>React Login Forms</h1>
+      </div>
+      <div>
+        <Login />
+      </div>
+    </>
+  );
+}
+
+export default App;
